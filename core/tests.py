4959a22d1f621ca9d5d56c18f1d2b43831f8d9b4
Carlos Goce <carlosgoce@gmail.com>
2015-01-31 12:58:11 +0100

    Add new service for reverse geocodes that returns data location for given coordinates

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import services
+
+
+class ReverseGeocodeTest(TestCase):
+    def test_returns_the_location_and_state_for_a_given_coordinates(self):
+        expected = {'postal_code': '36204', 'state': 'Galicia', 'city': 'Vigo',
+                    'county': 'Pontevedra', 'country': 'Spain'}
+
+        self.assertEqual(expected, services.reverse_geocode(42.228034, -8.719611))
