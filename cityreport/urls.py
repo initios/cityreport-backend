9c954fdd390cfa24bf94a5e30d6c308c563f3a52
Alex Hermida <alexhermida@gmail.com>
2015-01-31 14:55:43 +0100

    Add homepage

--- cityreport/urls.py
+++ cityreport/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 
-from core import views_api
+from core import views_api, views
 
 router = DefaultRouter()
 router.register(r'types', views_api.TypeViewSet)
@@ -11,6 +11,7 @@ router.register(r'issues', views_api.IssueViewSet)
 
 urlpatterns = patterns(
     '',
+     url(r'^$', views.index, name='index'),
     url(r'^api-rest/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
