446480e89ebd793fea391a7b69f2fa74d4c8f329
Carlos Goce <carlosgoce@gmail.com>
2015-01-31 11:33:59 +0100

    update model and migrations

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import model_utils.fields
 import django.utils.timezone
+import model_utils.fields
 
 
 class Migration(migrations.Migration):
@@ -15,14 +15,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Issue',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
-                ('created', model_utils.fields.AutoCreatedField(editable=False, verbose_name='created', default=django.utils.timezone.now)),
-                ('modified', model_utils.fields.AutoLastModifiedField(editable=False, verbose_name='modified', default=django.utils.timezone.now)),
-                ('lat', models.FloatField(verbose_name='Latitude', blank=True, null=True)),
-                ('lon', models.FloatField(verbose_name='Longitude', blank=True, null=True)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('created', model_utils.fields.AutoCreatedField(editable=False, default=django.utils.timezone.now, verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(editable=False, default=django.utils.timezone.now, verbose_name='modified')),
+                ('lat', models.FloatField(verbose_name='Latitude')),
+                ('lon', models.FloatField(verbose_name='Longitude')),
                 ('description', models.TextField(verbose_name='Description')),
-                ('location', models.CharField(verbose_name='Location', max_length=255)),
-                ('state', models.CharField(verbose_name='State', max_length=255)),
+                ('location', models.CharField(max_length=255, verbose_name='Location')),
+                ('state', models.CharField(max_length=255, verbose_name='State')),
             ],
             options={
                 'abstract': False,
@@ -32,10 +32,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Type',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
-                ('created', model_utils.fields.AutoCreatedField(editable=False, verbose_name='created', default=django.utils.timezone.now)),
-                ('modified', model_utils.fields.AutoLastModifiedField(editable=False, verbose_name='modified', default=django.utils.timezone.now)),
-                ('name', models.CharField(verbose_name='Name', max_length=255)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('created', model_utils.fields.AutoCreatedField(editable=False, default=django.utils.timezone.now, verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(editable=False, default=django.utils.timezone.now, verbose_name='modified')),
+                ('name', models.CharField(max_length=255, verbose_name='Name')),
             ],
             options={
                 'abstract': False,
--- core/models.py
+++ core/models.py
@@ -4,15 +4,15 @@ from model_utils.models import TimeStampedModel
 
 
 class Type(TimeStampedModel):
-    name = models.CharField(verbose_name=_('Name'), max_length=255, blank=False, null=False)
+    name = models.CharField(verbose_name=_('Name'), max_length=255)
 
 
 class Issue(TimeStampedModel):
     type = models.ForeignKey(Type, verbose_name=_('Type'))
 
-    lat = models.FloatField(_('Latitude'), blank=True, null=True)
-    lon = models.FloatField(_('Longitude'), blank=True, null=True)
+    lat = models.FloatField(_('Latitude'))
+    lon = models.FloatField(_('Longitude'))
     description = models.TextField(verbose_name=_('Description'))
-    location = models.CharField(verbose_name=_('Location'), max_length=255, blank=False, null=False)
-    state = models.CharField(verbose_name=_('State'), max_length=255, blank=False, null=False)
+    location = models.CharField(verbose_name=_('Location'), max_length=255)
+    state = models.CharField(verbose_name=_('State'), max_length=255)
 
