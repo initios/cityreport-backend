01dbf202fe2003e828913a9098972cd6b7405dcc
Carlos Goce <carlosgoce@gmail.com>
2015-01-31 11:29:04 +0100

    Add timestamped model and models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import ugettext as _
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Type(TimeStampedModel):
+    name = models.CharField(verbose_name=_('Name'), max_length=255, blank=False, null=False)
+
+
+class Issue(TimeStampedModel):
+    type = models.ForeignKey(Type, verbose_name=_('Type'))
+
+    lat = models.FloatField(_('Latitude'), blank=True, null=True)
+    lon = models.FloatField(_('Longitude'), blank=True, null=True)
+    description = models.TextField(verbose_name=_('Description'))
+    location = models.CharField(verbose_name=_('Location'), max_length=255, blank=False, null=False)
+    state = models.CharField(verbose_name=_('State'), max_length=255, blank=False, null=False)
+
