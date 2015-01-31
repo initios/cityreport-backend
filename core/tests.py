cb04aa9a9b7aa99a8d0c5470dc60ce9c05dbe66c
Carlos Goce <carlosgoce@gmail.com>
2015-01-31 13:02:12 +0100

    Add new test case

--- core/tests.py
+++ core/tests.py
@@ -9,3 +9,6 @@ class ReverseGeocodeTest(TestCase):
                     'county': 'Pontevedra', 'country': 'Spain'}
 
         self.assertEqual(expected, services.reverse_geocode(42.228034, -8.719611))
+
+    def test_if_coordinates_are_not_float_an_type_error_is_throwed(self):
+        self.assertRaises(TypeError, services.reverse_geocode, ['123', 'abc'])
